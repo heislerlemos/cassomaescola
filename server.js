4482d5a87a9abac3feaaaead953492e30abebdb6
heislerlemos <heisler.stlamo@gmail.com>
2022-08-17 10:37:20 +0100

    setting up new database

--- server.js
+++ server.js
@@ -1,100 +1,25 @@
-console.log('Cassoma escola');
-
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+//const MongoClient = require('mongodb').MongoClient
+const path = require('path'); 
+const connectDB = require('./server/database/connection');
+const studentRoute = require('./server/router/student');
 
 
+//app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
-MongoClient.connect("mongodb+srv://heisler:dude007@cluster0.oxiqyov.mongodb.net/?retryWrites=true&w=majority" ,{ useUnifiedTopology: true })
-  .then(client => {
-
-    console.log('connectado na base de dados')
-    const db = client.db('cassomaescola')
-    const studentCollection = db.collection('students')
+// Mongo connectiion
+connectDB();
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res) => {
+// routes inside the database connection
 
-  db.collection('students').find().toArray()
-    
-    .then(results => {
-          res.render('index.ejs', { students: results })
-      console.log(results)
-    })
-    
-    .catch(error => console.error(error))
-
-
-})
-
-app.put('/students', (req, res) => {
-
-    studentCollection.findOneAndUpdate(
-  { nome: 'heisler' },
-
-  {
-        $set: {
-      nome: req.body.nome,
-      turma: req.body.turma
-    }
-  },
-
-  {
-    upsert: true
-  }
-  
-)
-      .then(result => {
-               res.json(' Estudantes atualizados')
-       })
-    .catch(error => console.error(error))
-})
-
-
-
-    app.post('/students', (req, res) => {
-  
-  studentCollection.insertOne(req.body)
-
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => console.error(error))
-})
-
-
-app.delete('/students', (req, res) => {
-  studentCollection.deleteOne( 
-      { name: req.body.name }
-    )
-
-    .then(result => {
-      if (result.deletedCount === 0) {
-        return res.json('Não existe mais dados')
-      }
-      res.json(`Ficheiro foi apagado`)
-    })
-    .catch(error => console.error(error))
-})
-
-
-
-
-  })
-
-    
-
-
-
+app.use('/students', studentRoute  );
 
 app.listen(3000, function() {
   console.log('O servidor esta em produção na porta  3000')
---
+++ server/database/connection.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const connectDB = async() => {
+	try {
+		//mongodb connection string
+		const con = await mongoose.connect(`mongodb+srv://heisler:dude007@cluster0.oxiqyov.mongodb.net/students?retryWrites=true&w=majority`, {
+
+			useNewUrlParser:true,
+			useUnifiedTopology:true,
+			//useFindAndModify:false,
+			//useCreateIndex: true
+		})
+
+		console.log(`O servidor mongodb esta a connectado 💾 t)`);
+	}catch(err){
+
+		console.log(err);
+		process.exit(1);
+
+	}
+}
+
+module.exports = connectDB 
